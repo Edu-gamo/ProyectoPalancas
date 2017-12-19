3919883f1b94ee8c91b8f3fe5673fd8b99ed0043
Edu-gamo <eduard_gm94@hotmail.com>
2017-12-19 13:32:01 +0100

    Movimiento bolas

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float mass;
+
+    public Vector3 position;
+    public Vector3 postPosition;
+    public Vector3 positionInit;
+
+    public Vector3 velocity;
+    public Vector3 postVelocity;
+    public Vector3 velocityInit;
+
+    /*private float t;
+    private bool quepalo = false;
+    private float gravity = -9.81f;*/
+
+    // Use this for initialization
+    void Start () {
+
+        positionInit = this.transform.position;
+        position = this.transform.position;
+        velocityInit = new Vector3(0, 0, 0);
+        velocity = new Vector3(0, 0, 0);
+        //if (position.y == 0.1) velocity.y = -10;
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+
+        /*if (position.y > this.GetComponent<SphereCollider>().radius/2) {
+            t += Time.deltaTime;
+            velocity.y = velocityInit.y + gravity * t;
+            position.y = positionInit.y + velocityInit.y * t + (gravity / 2) * Mathf.Pow(t, 2);
+            if (!quepalo) quepalo = true;
+        } else if(quepalo) {
+            position.y = 0.1f;
+            quepalo = false;
+        }*/
+        this.transform.position = position;
+
+    }
+}
--- Assets/Scripts/IK_FABRIK.cs
+++ Assets/Scripts/IK_FABRIK.cs
@@ -21,88 +21,95 @@ public class IK_FABRIK : MonoBehaviour
         copy = new Vector3[joints.Length];
     }
 
-    void Update()
-    {
-        // Copy the joints positions to work with
-        // and calculate all the distances
-        //TODO1
-        for(int i = 0; i < joints.Length; i++) {
-            copy[i] = joints[i].position;
-            if(i < joints.Length - 1) distances[i] = (joints[i+1].position - joints[i].position).magnitude;
-        }
+    void Update() {
 
-        //done = TODO2
-        done = (target.position - joints[joints.Length - 1].position).magnitude < threshold_distance;
-        if (!done)
-        {
-            float targetRootDist = Vector3.Distance(copy[0], target.position);
+        if (target.position.y > 1.5f) {
 
-            // Update joint positions
-            if (targetRootDist > distances.Sum())
+            // Copy the joints positions to work with
+            // and calculate all the distances
+            //TODO1
+            for (int i = 0; i < joints.Length; i++)
             {
-                // The target is unreachable
-                //TODO3
-                for(int i = 0; i < copy.Length - 1; i++) {
-                    float dist = (target.position - copy[i]).magnitude;
-                    float lam = distances[i] / dist;
-                    copy[i + 1] = (1 - lam) * copy[i] + lam * target.position;
-                }
+                copy[i] = joints[i].position;
+                if (i < joints.Length - 1) distances[i] = (joints[i + 1].position - joints[i].position).magnitude;
             }
-            else
+
+            //done = TODO2
+            done = (target.position - joints[joints.Length - 1].position).magnitude < threshold_distance;
+            if (!done)
             {
-                // The target is reachable
-                //while (TODO4)
-                iter = 0;
-                while (!done /*|| iter < maxIter*/)
+                float targetRootDist = Vector3.Distance(copy[0], target.position);
+
+                // Update joint positions
+                if (targetRootDist > distances.Sum())
                 {
-                    // STAGE 1: FORWARD REACHING
-                    //TODO5
-                    copy[copy.Length - 1] = target.position;
-                    for (int i = copy.Length - 1; i > 0; i--) {
-                        Vector3 temp = (copy[i - 1] - copy[i]).normalized;
-                        temp = temp * distances[i - 1];
-                        copy[i - 1] = temp + copy[i];
+                    // The target is unreachable
+                    //TODO3
+                    for (int i = 0; i < copy.Length - 1; i++)
+                    {
+                        float dist = (target.position - copy[i]).magnitude;
+                        float lam = distances[i] / dist;
+                        copy[i + 1] = (1 - lam) * copy[i] + lam * target.position;
                     }
-
-                    // STAGE 2: BACKWARD REACHING
-                    //TODO6
-                    copy[0] = joints[0].position;
-                    for (int i = 0; i < copy.Length - 2; i++) {
-                        Vector3 temp = (copy[i + 1] - copy[i]).normalized;
-                        temp = temp * distances[i];
-                        copy[i + 1] = temp + copy[i];
-                    }
-
-                    done = (target.position - copy[copy.Length - 1]).magnitude < threshold_distance;
-                    iter++;
-
                 }
-                //Debug.Log(iter);
+                else
+                {
+                    // The target is reachable
+                    //while (TODO4)
+                    iter = 0;
+                    while (!done /*|| iter < maxIter*/)
+                    {
+                        // STAGE 1: FORWARD REACHING
+                        //TODO5
+                        copy[copy.Length - 1] = target.position;
+                        for (int i = copy.Length - 1; i > 0; i--)
+                        {
+                            Vector3 temp = (copy[i - 1] - copy[i]).normalized;
+                            temp = temp * distances[i - 1];
+                            copy[i - 1] = temp + copy[i];
+                        }
+
+                        // STAGE 2: BACKWARD REACHING
+                        //TODO6
+                        copy[0] = joints[0].position;
+                        for (int i = 0; i < copy.Length - 2; i++)
+                        {
+                            Vector3 temp = (copy[i + 1] - copy[i]).normalized;
+                            temp = temp * distances[i];
+                            copy[i + 1] = temp + copy[i];
+                        }
+
+                        done = (target.position - copy[copy.Length - 1]).magnitude < threshold_distance;
+                        iter++;
+
+                    }
+                    //Debug.Log(iter);
+                }
+
+                // Update original joint rotations
+                for (int i = 0; i <= joints.Length - 2; i++)
+                {
+                    //TODO7
+                    Vector3 a = joints[i + 1].position - joints[i].position;
+                    Vector3 b = copy[i + 1] - copy[i];
+                    Vector3 axis = Vector3.Cross(a, b).normalized;
+                    //  float angle = Mathf.Acos(Vector3.Dot(A, B) / (A.magnitude * B.magnitude)) * Mathf.Rad2Deg;
+                    //joints[i].Rotate(eix, angle, Space.World);
+
+
+                    float cosa = Vector3.Dot(a, b) / (a.magnitude * b.magnitude);
+                    float sina = Vector3.Cross(a.normalized, b.normalized).magnitude;
+
+                    float angle = Mathf.Atan2(sina, cosa);
+
+                    /*joints[i].rotation = Quaternion.AngleAxis(angle, eix) * joints[i].rotation;
+
+                    joints[i].position = copy[i];*/
+                    Quaternion q = new Quaternion(axis.x * Mathf.Sin(angle / 2), axis.y * Mathf.Sin(angle / 2), axis.z * Mathf.Sin(angle / 2), Mathf.Cos(angle / 2));
+                    joints[i].position = copy[i];
+                    joints[i].rotation = q * joints[i].rotation;
+                }
             }
-
-            // Update original joint rotations
-            for (int i = 0; i <= joints.Length - 2; i++)
-            {
-                //TODO7
-                Vector3 a = joints[i + 1].position - joints[i].position;
-                Vector3 b = copy[i + 1] - copy[i];
-                Vector3 axis = Vector3.Cross(a, b).normalized;
-                //  float angle = Mathf.Acos(Vector3.Dot(A, B) / (A.magnitude * B.magnitude)) * Mathf.Rad2Deg;
-                //joints[i].Rotate(eix, angle, Space.World);
-
-
-                float cosa = Vector3.Dot(a, b) / (a.magnitude * b.magnitude);
-                float sina = Vector3.Cross(a.normalized, b.normalized).magnitude;
-
-                float angle = Mathf.Atan2(sina, cosa);
-
-                /*joints[i].rotation = Quaternion.AngleAxis(angle, eix) * joints[i].rotation;
-
-                joints[i].position = copy[i];*/
-                Quaternion q = new Quaternion(axis.x * Mathf.Sin(angle / 2), axis.y * Mathf.Sin(angle / 2), axis.z * Mathf.Sin(angle / 2), Mathf.Cos(angle / 2));
-                joints[i].position = copy[i];
-                joints[i].rotation = q * joints[i].rotation;
-            }          
         }
     }
 
